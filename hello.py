a511560aaf6043b9484c1c6aded35e93566cd724
muzkeer <shaikmuzkeer008@gmail.com>
2025-08-31 17:30:00 +0530

    added feature

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
-###first code 
+##first code ##feature
 print("hello world")
 
-###random number 
+##random number 
 import random
 x = random.randrange(-9,9)
 if x > 0:
     print("positive")
 else:
     print("negative")
+
+
+
+
+
